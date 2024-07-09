9e308f2673fc0155fc1995f3617472ac68b367d1
BasavarajuVB <bellamvadde0393@gmailcom>
2024-07-09 10:39:30 +0000

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+// Write your JS code here
+import './index.css'
+
+const Header = () => (
+  <nav>
+    <div className="nav-responsive">
+      <div className="nav-content">
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+            className="website-logo-img"
+          />
+        </div>
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+            alt="nav logout"
+            className="nav-logout"
+          />
+        </div>
+      </div>
+    </div>
+  </nav>
+)
+export default Header
